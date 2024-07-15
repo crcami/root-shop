7b256de49bba8de0369731e7ca30904cc6ed3041
Camila Ramos <camilaafonsolemes@gmail.com>
2024-07-14 23:50:07 -0300

    fix(entrypoint): add echo and remove last cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@
 cd /var/www
 
 # Execute Composer Install
+echo "Running composer install..."
 composer install --no-interaction --optimize-autoloader
 
 # Execute migrations if the database is empty
+echo "Running migrations..."
 php artisan migrate --force
 
 # Populate the database if needed
+echo "Running seeds..."
 php artisan db:seed --force
-
-# Start php-fpm
-php-fpm
