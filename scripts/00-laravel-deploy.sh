58f82e958a8ac2880fc4e63f4c7c13e50fca1c10
Camila Afonso Lemes <camilaafonsolemes@gmail.com>
2024-07-21 11:06:24 -0300

    fix(deploy): add faker on laravel-deploy.sh and clean project

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
+composer require fakerphp/faker
 
 echo "Caching config..."
 php artisan config:cache
